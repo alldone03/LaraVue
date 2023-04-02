c513c63b7679692c4b042ea06edcd59a7ed205a9
alldone03 <aldanarsenal@gmail.com>
2023-04-02 17:12:33 +0700

    Data Validation (Back End & Front End)

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,34 +4,44 @@
       <div>
         <label for="">Beds</label>
         <input id="" v-model.number="form.beds" type="text" name="" />
+        <div v-if="form.errors.beds" class="">
+          {{ form.errors.beds }}
+        </div>
       </div>
       <div>
         <label for="">Baths</label>
         <input id="" v-model.number="form.baths" type="text" name="" />
+        <div v-if="form.errors.baths" class="">{{ form.errors.baths }}</div>
       </div>
       <div>
         <label for="">Area</label>
         <input id="" v-model.number="form.area" type="text" name="" />
+        <div v-if="form.errors.area" class="">{{ form.errors.area }}</div>
       </div>
       <div>
         <label for="">City</label>
         <input id="" v-model.number="form.city" type="text" name="" />
+        <div v-if="form.errors.number" class="">{{ form.errors.number }}</div>
       </div>
       <div>
         <label for="">Post Code</label>
         <input id="" v-model.number="form.code" type="text" name="" />
+        <div v-if="form.errors.code" class="">{{ form.errors.code }}</div>
       </div>
       <div>
         <label for="">Street</label>
         <input id="" v-model.number="form.street" type="text" name="" />
+        <div v-if="form.errors.street" class="">{{ form.errors.street }}</div>
       </div>
       <div>
         <label for="">Street Nr</label>
         <input id="" v-model.number="form.street_nr" type="text" name="" />
+        <div v-if="form.errors.street_nr" class="">{{ form.errors.street_nr }}</div>
       </div>
       <div>
         <label for="">Price</label>
         <input id="" v-model.number="form.price " type="text" name="" />
+        <div v-if="form.errors.price" class="">{{ form.errors.price }}</div>
       </div>
       <div>
         <button type="submit">Create</button>
@@ -42,7 +52,7 @@
 
 <script setup>
 
-// import { Inertia } from '@inertiajs/vue3'
+
 
 
 
